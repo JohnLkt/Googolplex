331a35f646948a86b8c6b2e544085b2c460a2bea
unknown <michaelangelo4489@gmail.com>
2024-12-02 02:25:40 +0700

    fonts, colors, responsives

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: '600px',
+        tablet: '800px',
+        laptop: '1000px',
+      },
+      colors: {
+        primary: '#292421',
+        secondary: '#7DF9FF',
+        accent: '#F1F1F2',
+      },
+      fontFamily: {
+        plusJakarta: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
