85da55b0e1af0d3dbbcfad14029ad000a88dbf99
unknown <michaelangelo4489@gmail.com>
2024-12-02 21:17:20 +0700

    configured existing interfaces for schema

---
+++ src/interfaces/GrandInterface.ts
@@ -0,0 +1,72 @@
+export interface User {
+  id?: string
+  email: string
+  password: string
+  name: string
+  picture: string
+  created_at: string
+  updated_at: string
+
+  notifications: Notification[]
+  user_class_member: UserClassMember[]
+}
+
+export interface Notification {
+  id?: string
+  title: string
+  description: string
+  created_at: string
+  updated_at: string
+  is_read: boolean
+  user_id: string
+
+  user: User
+}
+
+export interface Class {
+  id?: string
+  class_code: string
+  subject: string
+  description: string
+  created_at: string
+  updated_at: string
+}
+
+export interface UserClassMember {
+  id?: string
+  is_teacher: boolean
+  created_at: string
+  updated_at: string
+  class_id: string
+  user_id: string
+
+  class: Class
+  user: User
+}
+
+export interface Article {
+  id?: string
+  title: string
+  content: string
+  posts: Post[]
+}
+
+export interface Assignment {
+  id?: string
+  title: string
+  content: string
+  due_date: string
+  posts: Post[]
+}
+
+export interface Post {
+  id?: string
+  created_at: string
+  updated_at: string
+  article: Article
+  assignment?: Assignment
+  class: Class
+  article_id: string
+  class_id: string
+  assignment_id: string
+}
