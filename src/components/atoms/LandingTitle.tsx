b0259b328296dd4ce3f613c607943235a324457e
unknown <michaelangelo4489@gmail.com>
2024-12-02 23:05:41 +0700

    atoms: landing title

---
+++ src/components/atoms/LandingTitle.tsx
@@ -0,0 +1,7 @@
+export default function LandingTitle() {
+  return (
+    <div className="h-24 w-96 font-bold text-accent text-4xl">
+      Manage your educational pursuit with Googolplex
+    </div>
+  )
+}
