0b739ce710f6abf59997aa7b0c6baf5228d6e813
joheee <johevinblesstowi07@gmail.com>
2024-12-12 02:43:17 +0700

    feeds class detail

--- src/components/molecules/PostCard.tsx
+++ src/components/molecules/PostCard.tsx
@@ -1,19 +1,32 @@
 import { Post } from '../../interfaces/GrandInterface'
+import { format } from 'date-fns'
+
+function formatDate(date: string) {
+  return format(new Date(date), 'MMMM dd, yyyy hh:mm a')
+}
 
 const PostCard = (prop: Post) => {
-  console.log(prop)
+  const { article, assignment } = prop
 
   return (
-    <div className="z-10 cursor-pointer rounded-xl p-4 hover:scale-95 bg-gradient-to-br from-primary from-40% to-secondary hover:shadow-secondary hover:shadow-lg transition ease-in-out">
-      <div className="">
-        {prop.article?.title ? prop.article?.title : prop.assignment?.title}
-      </div>
-      <div className="">
-        {prop.article?.content
-          ? prop.article?.content
-          : prop.assignment?.content}
-      </div>
-      <div className="">{prop.assignment?.due_date ?? ''}</div>
+    <div className="z-10 cursor-pointer rounded-xl p-4 bg-gradient-to-br from-primary from-40% to-secondary hover:shadow-secondary hover:shadow-lg hover:scale-95 transition ease-in-out">
+      {article && (
+        <div className="mb-4">
+          <h2 className="text-lg font-bold">{article.title}</h2>
+          <p className="text-sm text-accent">{article.content}</p>
+        </div>
+      )}
+      {assignment && (
+        <div>
+          <h2 className="text-lg font-bold">{assignment.title}</h2>
+          <p className="text-sm text-accent">{assignment.content}</p>
+          {assignment.due_date && (
+            <p className="text-sm text-accent mt-2">
+              <strong>Due Date:</strong> {formatDate(assignment.due_date)}
+            </p>
+          )}
+        </div>
+      )}
     </div>
   )
 }
