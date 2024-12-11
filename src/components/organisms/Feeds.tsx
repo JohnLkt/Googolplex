8e78a31c82d8ed1e1956b9fdf0ec2fc2f8644b12
joheee <johevinblesstowi07@gmail.com>
2024-12-11 23:51:28 +0700

    custom hook for fetch post by classId

--- src/components/organisms/Feeds.tsx
+++ src/components/organisms/Feeds.tsx
@@ -1,3 +1,10 @@
+import { useParams } from 'react-router'
+import { useFetchPostByClassId } from '../../api/queries/Post'
+
 export default function Feeds() {
+  const { classId } = useParams()
+  const test = useFetchPostByClassId(classId!)
+  console.log(test.data)
+
   return <div>Feeds</div>
 }
