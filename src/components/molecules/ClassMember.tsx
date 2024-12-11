59f57b6e18c69447d2253f03b5a0018350e4a9e0
unknown <michaelangelo4489@gmail.com>
2024-12-11 23:57:14 +0700

    display class members

---
+++ src/components/molecules/ClassMember.tsx
@@ -0,0 +1,9 @@
+import { UserClassMember } from '../../interfaces/GrandInterface'
+
+export default function ClassMember({ ...classMember }: UserClassMember) {
+  return (
+    <div className="text-2xl text-accent font-plusJakarta">
+      {classMember.user.name}
+    </div>
+  )
+}
--- src/components/organisms/UserClassMemberList.tsx
+++ src/components/organisms/UserClassMemberList.tsx
@@ -1,7 +1,20 @@
-export default function UserClassMemberList() {
+import { useEffect } from 'react'
+import { useFetchClassMemberByClassId } from '../../api/queries/UserClassMember'
+import { Class } from '../../interfaces/GrandInterface'
+import ClassMember from '../molecules/ClassMember'
+
+export default function UserClassMemberList({ ...classProp }: Class) {
+  const { members } = useFetchClassMemberByClassId(classProp.id)
+
+  useEffect(() => {
+    if (members) {
+      console.log('members: ', members)
+    }
+  }, [members])
+
   return (
-    <div className="text-2xl text-accent font-plusJakarta">
-      UserClassMemberList
+    <div className="flex flex-col gap-3">
+      {members?.data.map((member) => <ClassMember {...member} />)}
     </div>
   )
 }
