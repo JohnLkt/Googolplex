f4bebb4986d924c154b640b998ea08ccc5896c31
John L <john.lukito@gmail.com>
2024-12-12 19:21:51 +0700

    fix: responsive dashboard page

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -10,11 +10,13 @@ export default function Sidebar() {
       id="sidebar"
       onMouseEnter={() => setSidebarWiden(true)}
       onMouseLeave={() => setSidebarWiden(false)}
-      className={`z-20 transition-all duration-300 ${
-        sidebarWiden ? 'w-1/5 max-mobile:w-[240px]' : 'w-16'
-      } bg-primary p-4 border-r-2 border-accent overflow-auto`}
+      className={`z-20 transition-hover duration-300 ${
+        sidebarWiden
+          ? 'w-1/5 max-mobile:w-full max-mobile:absolute max-mobile:inset-0 max-mobile:z-40 border-r-2'
+          : 'w-16 max-mobile:w-0 max-mobile:p-0 '
+      } bg-primary p-4 max-mobile:p-0 border-accent overflow-auto border-r-2 max-mobile:border-r-0`}
     >
-      <div className="flex flex-col space-y-6">
+      <div className="flex flex-col space-y-6 max-mobile:m-4">
         <SidebarItem
           sidebarTitle="Home"
           sidebarWiden={sidebarWiden}
