0418fe1e5f408fbf581154e2ee62c5a372fa45aa
joheee <johevinblesstowi07@gmail.com>
2024-12-15 02:00:26 +0700

    TODO PAGE

--- src/components/organisms/AssignmentList.tsx
+++ src/components/organisms/AssignmentList.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router'
 import { Assignment } from '../../interfaces/GrandInterface'
 import ActionLightButton from '../atoms/ActionLightButton'
 import PostCard from '../molecules/PostCard'
+import AuthTeacherWrapper from '../molecules/AuthTeacherWrapper'
 
 interface AssignmentListProps {
   assignments: Assignment[]
@@ -11,14 +12,16 @@ export default function AssignmentList({ assignments }: AssignmentListProps) {
   const navigate = useNavigate()
   return (
     <div className="w-full flex flex-col gap-3 p-6">
-      <div
-        className="w-fit"
-        onClick={() => {
-          navigate('add/assignment/')
-        }}
-      >
-        <ActionLightButton actionText="Add Assignment" />
-      </div>
+      <AuthTeacherWrapper>
+        <div
+          className="w-fit"
+          onClick={() => {
+            navigate('add/assignment/')
+          }}
+        >
+          <ActionLightButton actionText="Add Assignment" />
+        </div>
+      </AuthTeacherWrapper>
       {assignments &&
         assignments.map((assignment, i) => (
           <PostCard assignment={assignment} key={i} />
